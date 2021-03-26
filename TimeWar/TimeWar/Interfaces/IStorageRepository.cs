d186d16f0bf914cb4ef36c36b49d8346980f187d
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2021-03-26 20:03:44 +0100

    Added interfaces.

---
+++ TimeWar/TimeWar/Interfaces/IStorageRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeWar.Interfaces
+{
+    public interface IStorageRepository
+    {
+        void SaveGame();
+
+        void LoadGame();
+
+        void StoreHighScore();
+
+        void LoadHighScore();
+    }
+}
