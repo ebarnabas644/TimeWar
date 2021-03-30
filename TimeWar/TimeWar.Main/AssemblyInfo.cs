12a3052bafb50f41f9839956b75ba5079236fc5c
Erdei Barnabás <ebarnabas@stud.uni-obuda.hu>
2021-03-30 22:45:06 +0200

    Starting DLLs created

---
+++ TimeWar/TimeWar.Main/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+// <copyright file="AssemblyInfo.cs" company="Time War">
+// Copyright (c) Time War. All rights reserved.
+// </copyright>
+
+using System;
+using System.Windows;
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: CLSCompliant(false)]
---
+++ TimeWar/TimeWar.Main/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="MainWindow.xaml.cs" company="Time War">
+// Copyright (c) Time War. All rights reserved.
+// </copyright>
+
+namespace TimeWar.Main
+{
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
