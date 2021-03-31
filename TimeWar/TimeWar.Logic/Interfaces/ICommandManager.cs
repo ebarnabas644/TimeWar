add7a67d2bb6e3a8c8626f1f9521ed7f0093f9e4
Erdei Barnabás <ebarnabas@stud.uni-obuda.hu>
2021-04-01 01:01:31 +0200

    Command manager interface

---
+++ TimeWar/TimeWar.Logic/Interfaces/ICommandManager.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="ICommandManager.cs" company="Time War">
+// Copyright (c) Time War. All rights reserved.
+// </copyright>
+
+namespace TimeWar.Logic.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Command manager interface.
+    /// </summary>
+    public interface ICommandManager
+    {
+        /// <summary>
+        /// Add new command.
+        /// </summary>
+        /// <param name="command">Command.</param>
+        void AddCommand(ICommand command);
+
+        /// <summary>
+        /// Clear command buffer.
+        /// </summary>
+        void ClearBuffer();
+
+        /// <summary>
+        /// Rewind all command.
+        /// </summary>
+        void Rewind();
+    }
+}
