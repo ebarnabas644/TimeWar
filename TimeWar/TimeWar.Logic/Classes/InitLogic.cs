a5e9531ea1250b3ebe063fc64449b305b048f8e3
Erdei Barnabás <ebarnabas@stud.uni-obuda.hu>
2021-04-04 16:03:51 +0200

    Bad ground recognititon fixed and extra player parameters added for size.

--- TimeWar/TimeWar.Logic/Classes/InitLogic.cs
+++ TimeWar/TimeWar.Logic/Classes/InitLogic.cs
@@ -55,7 +55,7 @@ namespace TimeWar.Logic
             {
                 for (int x = 0; x < gameWorld.GetTileWidth; x++)
                 {
-                    if (grounddata[y][x] == "1")
+                    if (grounddata[y][x] != "0")
                     {
                         gameWorld.AddGround(new Point(x, y));
                     }
@@ -83,7 +83,7 @@ namespace TimeWar.Logic
         {
             int startX = this.model.CurrentWorld.SearchPointOfInterest("start").X * this.model.CurrentWorld.Magnify * this.model.CurrentWorld.TileSize;
             int startY = this.model.CurrentWorld.SearchPointOfInterest("start").Y * this.model.CurrentWorld.Magnify * this.model.CurrentWorld.TileSize;
-            return new Player(new Point(startX, startY), this.model.CurrentWorld.TileSize, 100);
+            return new Player(new Point(startX, startY), this.model.CurrentWorld.TileSize, 100, 32, 16);
         }
 
         private void BuildModel(string mapName)
