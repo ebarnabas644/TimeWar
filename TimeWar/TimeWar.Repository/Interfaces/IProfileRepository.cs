e09910f2afbfa4bbc82dcb1de7c1631c6e450bda
Erdei Barnabás <ebarnabas@stud.uni-obuda.hu>
2021-04-06 12:34:12 +0200

    Entity class interfaces added.

---
+++ TimeWar/TimeWar.Repository/Interfaces/IProfileRepository.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="IProfileRepository.cs" company="Time War">
+// Copyright (c) Time War. All rights reserved.
+// </copyright>
+
+namespace TimeWar.Repository.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using TimeWar.Data.Models;
+
+    /// <summary>
+    /// Extra operations for Profile class entities.
+    /// </summary>
+    public interface IProfileRepository : IMainRepository<Profile>
+    {
+        /// <summary>
+        /// Update entity content.
+        /// </summary>
+        /// <param name="entity">Profile entity object.</param>
+        void Update(Profile entity);
+    }
+}
