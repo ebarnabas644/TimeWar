470be76a1dd6c9160025486308d17c88fe7759b9
Erdei Barnabás <ebarnabas@stud.uni-obuda.hu>
2021-04-06 12:33:39 +0200

    Main repository interface added.

---
+++ TimeWar/TimeWar.Repository/Interfaces/IMainRepository.cs
@@ -0,0 +1,41 @@
+﻿// <copyright file="IMainRepository.cs" company="Time War">
+// Copyright (c) Time War. All rights reserved.
+// </copyright>
+
+namespace TimeWar.Repository.Interfaces
+{
+    using System.Linq;
+
+    /// <summary>
+    /// Generic repository interface for common operations.
+    /// </summary>
+    /// <typeparam name="T">Entity class.</typeparam>
+    public interface IMainRepository<T>
+        where T : class
+    {
+        /// <summary>
+        /// Get one entity via id.
+        /// </summary>
+        /// <param name="id">Id of the entity.</param>
+        /// <returns>Return the entity object based on id.</returns>
+        T GetOne(int id);
+
+        /// <summary>
+        /// Get all element from table.
+        /// </summary>
+        /// <returns>Entites.</returns>
+        IQueryable<T> GetAll();
+
+        /// <summary>
+        /// Add entity to the table.
+        /// </summary>
+        /// <param name="entity">Entity object.</param>
+        void Create(T entity);
+
+        /// <summary>
+        /// Delete entity from the table.
+        /// </summary>
+        /// <param name="entity">Entity object.</param>
+        void Delete(T entity);
+    }
+}
