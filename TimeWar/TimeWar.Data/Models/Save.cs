ea3cbfd70c7ea09185c98372fae1d535f8e72daa
Erdei Barnabás <ebarnabas@stud.uni-obuda.hu>
2021-04-06 12:32:52 +0200

    Save entity class added.

---
+++ TimeWar/TimeWar.Data/Models/Save.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="Save.cs" company="Time War">
+// Copyright (c) Time War. All rights reserved.
+// </copyright>
+
+namespace TimeWar.Data.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Game save entity class.
+    /// </summary>
+    public class Save
+    {
+        /// <summary>
+        /// Gets or sets save id.
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets point.
+        /// </summary>
+        public int Point { get; set; }
+
+        /// <summary>
+        /// Gets or sets checkpoint.
+        /// </summary>
+        public int Checkpoint { get; set; }
+
+        /// <summary>
+        /// Gets or sets player navigational property.
+        /// </summary>
+        public virtual Profile Player { get; set; }
+    }
+}
