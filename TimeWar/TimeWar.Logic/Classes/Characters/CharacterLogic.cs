366bc049c9844a19a6a263997762a05f3d297b7c
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2021-04-07 23:07:37 +0200

    Added collision

--- TimeWar/TimeWar.Logic/Classes/Characters/CharacterLogic.cs
+++ TimeWar/TimeWar.Logic/Classes/Characters/CharacterLogic.cs
@@ -9,6 +9,7 @@ namespace TimeWar.Logic
     using System.Drawing;
     using System.Linq;
     using System.Text;
+    using System.Threading;
     using System.Threading.Tasks;
     using TimeWar.Logic.Classes;
     using TimeWar.Logic.Classes.Characters.Actions;
@@ -24,6 +25,7 @@ namespace TimeWar.Logic
         private GameModel model;
         private Character character;
         private CommandManager commandManager;
+        private bool isJumping; 
 
         /// <summary>
         /// Initializes a new instance of the <see cref="CharacterLogic"/> class.
@@ -36,6 +38,7 @@ namespace TimeWar.Logic
             this.model = model;
             this.character = character;
             this.commandManager = commandManager;
+            this.isJumping = false;
         }
 
         /// <summary>
@@ -43,9 +46,40 @@ namespace TimeWar.Logic
         /// </summary>
         public void OneTick()
         {
-            MoveCommand command = new MoveCommand(this.character, this.Move(), this.model);
-            command.Execute();
-            this.commandManager.AddCommand(command);
+            bool commandAdded = false;
+            Point newPoint = this.Move();
+
+            if (!this.Collision(newPoint))
+            {
+                MoveCommand command = new MoveCommand(this.character, newPoint, this.model);
+                command.Execute();
+                if (newPoint.Y == -1)
+                {
+                    int counter = 0;
+                    while (counter < 14 && !this.Collision(newPoint))
+                    {
+                        command = new MoveCommand(this.character, newPoint, this.model);
+                        command.Execute();
+                        counter++;
+                        this.commandManager.AddCommand(command);
+                    }
+
+                    commandAdded = true;
+                }
+
+                if (!this.Collision(new Point(0, 1)))
+                {
+                    MoveCommand gravity = new MoveCommand(this.character, new Point(0, 1), this.model);
+                    gravity.Execute();
+                }
+
+                if (!commandAdded)
+                {
+                    this.commandManager.AddCommand(command);
+                }
+            }
+
+
         }
 
         private Point Move()
@@ -63,7 +97,13 @@ namespace TimeWar.Logic
                     direction = new Point(-1, 0);
                     break;
                 case Stances.Up:
-                    direction = new Point(0, -1);
+                    direction = new Point(0, 0);
+                    if (!this.isJumping)
+                    {
+                        this.isJumping = true;
+                        direction = new Point(0, -1);
+                    }
+
                     break;
                 case Stances.Down:
                     direction = new Point(0, 1);
@@ -74,6 +114,61 @@ namespace TimeWar.Logic
             }
 
             return direction;
+        }
+
+        private bool Collision(Point newPoint)
+        {
+            Rectangle actor = new Rectangle(
+                this.character.Position.X + newPoint.X,
+                this.character.Position.Y + newPoint.Y,
+                this.character.Width / 8,
+                this.character.Height / 8);
+
+            Point actorLocation;
+
+            for (int i = 0; i < actor.Width; i++)
+            {
+                actorLocation = new Point(this.PixelToTile(actor.X) + i, this.PixelToTile(actor.Y - 1) + actor.Height);
+                if (this.model.CurrentWorld.SearchGround(actorLocation))
+                {
+                    this.isJumping = false;
+                    return true;
+                }
+            }
+
+            for (int i = 0; i < actor.Height; i++)
+            {
+                actorLocation = new Point(this.PixelToTile(actor.X - 1) + actor.Width, this.PixelToTile(actor.Y) + i);
+                if (this.model.CurrentWorld.SearchGround(actorLocation))
+                {
+                    return true;
+                }
+            }
+
+            for (int i = 0; i < actor.Width; i++)
+            {
+                actorLocation = new Point(this.PixelToTile(actor.X) + i, this.PixelToTile(actor.Y));
+                if (this.model.CurrentWorld.SearchGround(actorLocation))
+                {
+                    return true;
+                }
+            }
+
+            for (int i = 0; i < actor.Height; i++)
+            {
+                actorLocation = new Point(this.PixelToTile(actor.X), this.PixelToTile(actor.Y) + i);
+                if (this.model.CurrentWorld.SearchGround(actorLocation))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private int PixelToTile(int num)
+        {
+            return this.model.CurrentWorld.ConvertPixelToTile(num);
         }
     }
 }
