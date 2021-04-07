ad12181d60dd48674a3c6bb75b52733a0f596b2d
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2021-04-07 23:08:19 +0200

    Added Tests class

---
+++ TimeWar/TimeWar.LogicTests/Tests.cs
@@ -0,0 +1,30 @@
+﻿namespace TimeWar.LogicTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Moq;
+    using NUnit.Framework;
+    using TimeWar.Logic.Interfaces;
+
+    /// <summary>
+    /// Test class for logic methods.
+    /// </summary>
+    public class Tests
+    {
+        private Mock<ICommand> mockedCommand;
+        private Mock<ICommandManager> mockedCommandManager;
+
+        /// <summary>
+        /// Sets up testing.
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            this.mockedCommand = new Mock<ICommand>(MockBehavior.Loose);
+            this.mockedCommandManager = new Mock<ICommandManager>(MockBehavior.Loose);
+        }
+    }
+}
