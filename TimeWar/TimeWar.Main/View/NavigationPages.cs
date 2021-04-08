9ddd1d8b80cf6786af5e2c24fd796c849985d403
Erdei Barnabás <ebarnabas@stud.uni-obuda.hu>
2021-04-08 17:08:31 +0200

    Pages enum.

---
+++ TimeWar/TimeWar.Main/View/NavigationPages.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="NavigationPages.cs" company="Time War">
+// Copyright (c) Time War. All rights reserved.
+// </copyright>
+
+namespace TimeWar.Main.View
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public enum NavigationPages
+    {
+        MenuPage,
+        GamePage,
+        ProfilesPage
+    }
+}
