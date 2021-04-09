ee72d0bb509f16735ebc7023c97d19b2523bcf44
Erdei Barnabás <ebarnabas@stud.uni-obuda.hu>
2021-04-09 21:54:22 +0200

    Menu page design.

--- TimeWar/TimeWar.Main/View/MenuPage.xaml.cs
+++ TimeWar/TimeWar.Main/View/MenuPage.xaml.cs
@@ -1,28 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿// <copyright file="MenuPage.xaml.cs" company="Time War">
+// Copyright (c) Time War. All rights reserved.
+// </copyright>
 
 namespace TimeWar.Main.View
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+
     /// <summary>
-    /// Interaction logic for MenuPage.xaml
+    /// Interaction logic for MenuPage.xaml.
     /// </summary>
     public partial class MenuPage : Page
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MenuPage"/> class.
+        /// </summary>
         public MenuPage()
         {
-            InitializeComponent();
+            this.InitializeComponent();
         }
     }
 }
