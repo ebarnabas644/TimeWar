71d7a61ffc003ada9e0a11ee983413560a8e2247
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2021-04-15 15:05:29 +0200

    Added movement to enemy logic

--- TimeWar/TimeWar.Logic/Classes/Characters/EnemyLogic.cs
+++ TimeWar/TimeWar.Logic/Classes/Characters/EnemyLogic.cs
@@ -6,8 +6,10 @@ namespace TimeWar.Logic.Classes.Characters
 {
     using System;
     using System.Collections.Generic;
+    using System.Diagnostics;
     using System.Drawing;
     using System.Linq;
+    using System.Security.Cryptography;
     using System.Text;
     using System.Threading.Tasks;
     using TimeWar.Model;
@@ -18,6 +20,16 @@ namespace TimeWar.Logic.Classes.Characters
     /// </summary>
     public class EnemyLogic : ActorLogic
     {
+        private const int DetectionTime = 60;
+        private Stopwatch movementDirStopwatch;
+        private Stopwatch movementStopwatch;
+        private int movementDirTime;
+        private int movementTime;
+        private int moveDir;
+        private Point lastKnownPlayerLocation;
+        private bool isPlayerDetected;
+        private Stopwatch playerDetectionTime;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="EnemyLogic"/> class.
         /// </summary>
@@ -27,18 +39,15 @@ namespace TimeWar.Logic.Classes.Characters
         public EnemyLogic(GameModel model, Character character, CommandManager commandManager)
             : base(model, character, commandManager)
         {
-        }
-
-        /// <inheritdoc/>
-        public override bool Equals(object obj)
-        {
-            return base.Equals(obj);
-        }
-
-        /// <inheritdoc/>
-        public override int GetHashCode()
-        {
-            return base.GetHashCode();
+            this.movementDirStopwatch = new Stopwatch();
+            this.movementDirStopwatch.Start();
+            this.movementStopwatch = new Stopwatch();
+            this.movementDirTime = RandomNumberGenerator.GetInt32(7000);
+            this.movementTime = RandomNumberGenerator.GetInt32(7000);
+            this.moveDir = 0;
+            this.lastKnownPlayerLocation = new Point(0, 0);
+            this.isPlayerDetected = false;
+            this.playerDetectionTime = new Stopwatch();
         }
 
         /// <inheritdoc/>
@@ -48,21 +57,88 @@ namespace TimeWar.Logic.Classes.Characters
         }
 
         /// <inheritdoc/>
-        public override string ToString()
-        {
-            return base.ToString();
-        }
-
-        /// <inheritdoc/>
         protected override Point Move()
         {
-            return base.Move();
+            int x = 0;
+            int y = 0;
+            if (!this.isPlayerDetected)
+            {
+                if (this.movementDirStopwatch.ElapsedMilliseconds > this.movementDirTime)
+                {
+                    this.moveDir = RandomNumberGenerator.GetInt32(4);
+                    this.movementDirTime = RandomNumberGenerator.GetInt32(7000);
+                    this.movementDirStopwatch.Restart();
+                }
+
+                switch (this.moveDir)
+                {
+                    case 0:
+                        x -= 2;
+                        break;
+
+                    case 1:
+                        if (!this.IsJumping && this.JumpingTimeOut.ElapsedMilliseconds > 250)
+                        {
+                            this.JumpingTimeOut.Restart();
+                            this.IsJumping = true;
+                            this.AccelerationStopwatch.Start();
+                            this.moveDir = RandomNumberGenerator.GetInt32(4);
+                            this.movementDirStopwatch.Restart();
+                            y -= this.MaxJumpHeight;
+                        }
+
+                        break;
+
+                    case 2:
+                        x += 2;
+                        break;
+
+                    case 3:
+                        x += 0;
+                        break;
+
+                    default:
+                        x += 0;
+                        break;
+                }
+            }
+            else
+            {
+                x = 0;
+                y = 0;
+                if (this.Character.Position.X > this.lastKnownPlayerLocation.X)
+                {
+                    x -= 2;
+                }
+
+                if (this.Character.Position.X < this.lastKnownPlayerLocation.X)
+                {
+                    x += 2;
+                }
+
+                //lastKnownPlayerLocation.Y;
+            }
+
+            return new Point(x, y);
         }
 
         /// <inheritdoc/>
         protected override void Movement()
         {
-            base.Movement();
+            if (this.movementStopwatch.ElapsedMilliseconds < this.movementTime)
+            {
+                base.Movement();
+            }
+            else
+            {
+                this.movementStopwatch.Reset();
+                this.movementTime = RandomNumberGenerator.GetInt32(7000);
+            }
+        }
+
+        private void DetectPlayer()
+        {
+            throw new NotImplementedException();
         }
     }
 }
