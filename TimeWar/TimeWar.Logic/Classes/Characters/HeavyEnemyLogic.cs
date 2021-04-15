aec5dde76814e65d34cc2b45b8a21ef3814979ad
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2021-04-15 21:23:02 +0200

    Added enemy types.

---
+++ TimeWar/TimeWar.Logic/Classes/Characters/HeavyEnemyLogic.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="HeavyEnemyLogic.cs" company="Time War">
+// Copyright (c) Time War. All rights reserved.
+// </copyright>
+
+namespace TimeWar.Logic.Classes.Characters
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using TimeWar.Model;
+    using TimeWar.Model.Objects;
+
+    /// <summary>
+    /// Heavy enemy.
+    /// </summary>
+    public class HeavyEnemyLogic : BasicEnemyLogic
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HeavyEnemyLogic"/> class.
+        /// </summary>
+        /// <param name="model">Game model.</param>
+        /// <param name="character">Character.</param>
+        /// <param name="commandManager">Command manager.</param>
+        public HeavyEnemyLogic(GameModel model, Character character, CommandManager commandManager)
+            : base(model, character, commandManager)
+        {
+            this.Character.Health = 200;
+            this.MaxMovementSpeed = 7;
+            this.MaxJumpHeight = 15;
+        }
+    }
+}
