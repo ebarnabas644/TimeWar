eed33a6e6f95a4cdf2ea941d296ca3a1472b5b2c
Erdei Barnabás <ebarnabas@stud.uni-obuda.hu>
2021-04-18 21:50:46 +0200

    Profiles page actions added.

--- TimeWar/TimeWar.Main/View/ProfilesPage.xaml.cs
+++ TimeWar/TimeWar.Main/View/ProfilesPage.xaml.cs
@@ -4,32 +4,43 @@
 
 namespace TimeWar.Main.View
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Windows;
     using System.Windows.Controls;
-    using System.Windows.Data;
-    using System.Windows.Documents;
-    using System.Windows.Input;
-    using System.Windows.Media;
-    using System.Windows.Media.Imaging;
-    using System.Windows.Navigation;
-    using System.Windows.Shapes;
+    using TimeWar.Main.ViewModel;
 
     /// <summary>
     /// Interaction logic for ProfilesPage.xaml.
     /// </summary>
     public partial class ProfilesPage : Page
     {
+        private ProfilesViewModel vm;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="ProfilesPage"/> class.
         /// </summary>
         public ProfilesPage()
         {
             this.InitializeComponent();
+            this.vm = this.DataContext as ProfilesViewModel;
+            MenuControl menuControl = this.FindName("cont") as MenuControl;
+            Grid a = this.FindName("MainGrid") as Grid;
+            menuControl.MapName = "test2";
+            menuControl.ScrollMode = false;
+            menuControl.TitleEnabled = false;
+        }
+
+        private void Add_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.vm.Editing = new Data.PlayerProfileUI();
+            this.AddDialog.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void Modify_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            if (this.vm.SelectedPlayer != null)
+            {
+                this.vm.Editing = this.vm.SelectedPlayer;
+                this.EditDialog.Visibility = System.Windows.Visibility.Visible;
+            }
         }
     }
 }
--- TimeWar/TimeWar.Main/ViewModel/ProfilesViewModel.cs
+++ TimeWar/TimeWar.Main/ViewModel/ProfilesViewModel.cs
@@ -4,36 +4,145 @@
 
 namespace TimeWar.Main.ViewModel
 {
-    using System;
-    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    using CommonServiceLocator;
+    using GalaSoft.MvvmLight;
     using GalaSoft.MvvmLight.Command;
+    using GalaSoft.MvvmLight.Ioc;
+    using TimeWar.Main.BL.Interfaces;
+    using TimeWar.Main.Data;
     using TimeWar.Main.View;
 
     /// <summary>
     /// Profile view model class.
     /// </summary>
-    public class ProfilesViewModel
+    public class ProfilesViewModel : ViewModelBase
     {
         private INavigationService<NavigationPages> navigationService;
-        private RelayCommand menuPageCommand;
+        private ObservableCollection<PlayerProfileUI> playerProfileUIs;
+        private IViewerLogicUI viewerLogicUI;
+        private IManagerLogicUI managerLogicUI;
+        private PlayerProfileUI selectedPlayer;
+        private PlayerProfileUI editing;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ProfilesViewModel"/> class.
         /// </summary>
         /// <param name="navigationService">Navigation service.</param>
-        public ProfilesViewModel(INavigationService<NavigationPages> navigationService)
+        /// <param name="viewerLogicUI">Viewer logic.</param>
+        /// <param name="managerLogicUI">Manager logic.</param>
+        public ProfilesViewModel(INavigationService<NavigationPages> navigationService, IViewerLogicUI viewerLogicUI, IManagerLogicUI managerLogicUI)
         {
+            this.playerProfileUIs = new ObservableCollection<PlayerProfileUI>();
+            this.editing = new PlayerProfileUI();
             this.navigationService = navigationService;
+            this.viewerLogicUI = viewerLogicUI;
+            this.managerLogicUI = managerLogicUI;
+            if (this.IsInDesignMode)
+            {
+            }
+            else
+            {
+                this.viewerLogicUI.GetProfiles().ToList().ForEach(x => this.playerProfileUIs.Add(x));
+                this.MenuPageCommand = new RelayCommand(() =>
+                {
+                    if (this.SelectedPlayer != null)
+                    {
+                        PlayerProfileUI deselected = this.viewerLogicUI.GetSelectedProfile();
+                        if (deselected != null)
+                        {
+                            deselected.Selected = false;
+                            this.managerLogicUI.ModifyProfile(deselected);
+                        }
+
+                        this.SelectedPlayer.Selected = true;
+                        this.managerLogicUI.ModifyProfile(this.selectedPlayer);
+                    }
+
+                    this.navigationService.NavigateTo("MenuPage");
+                });
+                this.CreateProfileCommand = new RelayCommand(() =>
+                {
+                    this.managerLogicUI.CreateProfile(this.PlayerProfileUIs, this.editing);
+                    this.playerProfileUIs.Clear();
+                    this.viewerLogicUI.GetProfiles().ToList().ForEach(x => this.playerProfileUIs.Add(x));
+                    this.editing = new PlayerProfileUI();
+                });
+                this.ModifyProfileCommand = new RelayCommand(() =>
+                {
+                    this.managerLogicUI.ModifyProfile(this.editing);
+                    this.editing = new PlayerProfileUI();
+                });
+                this.DeleteProfileCommand = new RelayCommand(() =>
+                {
+                    this.managerLogicUI.DeleteProfile(this.playerProfileUIs, this.selectedPlayer);
+                    this.editing = new PlayerProfileUI();
+                });
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProfilesViewModel"/> class.
+        /// </summary>
+        [PreferredConstructor]
+        public ProfilesViewModel()
+            : this(IsInDesignModeStatic ? null : ServiceLocator.Current.GetInstance<INavigationService<NavigationPages>>(), IsInDesignModeStatic ? null : ServiceLocator.Current.GetInstance<IViewerLogicUI>(), IsInDesignModeStatic ? null : ServiceLocator.Current.GetInstance<IManagerLogicUI>())
+        {
         }
 
         /// <summary>
         /// Gets the navigate to game page command.
         /// </summary>
-        public RelayCommand MenuPageCommand => this.menuPageCommand
-                    ?? (this.menuPageCommand = new RelayCommand(
-                    () => this.navigationService.NavigateTo("MenuPage")));
+        public RelayCommand MenuPageCommand { get; private set; }
+
+        /// <summary>
+        /// Gets the create profile command.
+        /// </summary>
+        public RelayCommand CreateProfileCommand { get; private set; }
+
+        /// <summary>
+        /// Gets the create profile command.
+        /// </summary>
+        public RelayCommand ModifyProfileCommand { get; private set; }
+
+        /// <summary>
+        /// Gets the create profile command.
+        /// </summary>
+        public RelayCommand DeleteProfileCommand { get; private set; }
+
+        /// <summary>
+        /// Gets or sets currently selected player.
+        /// </summary>
+        public PlayerProfileUI SelectedPlayer
+        {
+            get { return this.selectedPlayer; }
+            set { this.Set(ref this.selectedPlayer, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets editing instance.
+        /// </summary>
+        public PlayerProfileUI Editing
+        {
+            get { return this.editing; }
+            set { this.Set(ref this.editing, value); }
+        }
+
+        /// <summary>
+        /// Gets the player profiles collection.
+        /// </summary>
+        public ObservableCollection<PlayerProfileUI> PlayerProfileUIs
+        {
+            get
+            {
+                return this.playerProfileUIs;
+            }
+
+            private set
+            {
+                this.playerProfileUIs = value;
+            }
+        }
     }
 }
