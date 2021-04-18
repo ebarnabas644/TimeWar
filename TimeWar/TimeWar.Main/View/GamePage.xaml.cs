cb5c924df22576078eac188df364c0c3fccbeadd
Erdei Barnabás <ebarnabas@stud.uni-obuda.hu>
2021-04-18 21:49:16 +0200

    Game page map parameter set.

--- TimeWar/TimeWar.Main/View/GamePage.xaml.cs
+++ TimeWar/TimeWar.Main/View/GamePage.xaml.cs
@@ -4,32 +4,25 @@
 
 namespace TimeWar.Main.View
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Windows;
     using System.Windows.Controls;
-    using System.Windows.Data;
-    using System.Windows.Documents;
-    using System.Windows.Input;
-    using System.Windows.Media;
-    using System.Windows.Media.Imaging;
-    using System.Windows.Navigation;
-    using System.Windows.Shapes;
+    using TimeWar.Main.ViewModel;
 
     /// <summary>
     /// Interaction logic for GameWindow.xaml.
     /// </summary>
     public partial class GameWindow : Page
     {
+        private GameViewModel vm;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="GameWindow"/> class.
         /// </summary>
         public GameWindow()
         {
             this.InitializeComponent();
+            this.vm = this.DataContext as GameViewModel;
+            GameControl gc = this.FindName("cont") as GameControl;
+            gc.MapName = (string)this.vm.NavigationContext.Parameter;
         }
     }
 }
