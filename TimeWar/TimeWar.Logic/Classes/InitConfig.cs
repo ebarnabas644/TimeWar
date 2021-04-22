3c3d2ae9aa8f0a146e62e8c75d3bc8e675fd82e0
Erdei Barnabás <ebarnabas@stud.uni-obuda.hu>
2021-04-22 22:09:43 +0200

    Basic enemy init config added.

--- TimeWar/TimeWar.Logic/Classes/InitConfig.cs
+++ TimeWar/TimeWar.Logic/Classes/InitConfig.cs
@@ -28,5 +28,25 @@ namespace TimeWar.Logic.Classes
         /// Play character health const.
         /// </summary>
         public const int PlayerHealth = 100;
+
+        /// <summary>
+        /// Name of the basic enemy spritesheet.
+        /// </summary>
+        public const string BasicEnemySpritesheet = "testenemy";
+
+        /// <summary>
+        /// Basic enemy height const.
+        /// </summary>
+        public const int BasicEnemyHeight = 32;
+
+        /// <summary>
+        /// Basic enemy width const.
+        /// </summary>
+        public const int BasicEnemyWidth = 16;
+
+        /// <summary>
+        /// Basic enemy health const.
+        /// </summary>
+        public const int BasicEnemyHealth = 50;
     }
 }
