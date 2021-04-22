44e69521c3e98b624d433a427018ff729be0e3cc
Erdei Barnabás <ebarnabas@stud.uni-obuda.hu>
2021-04-22 22:11:21 +0200

    Enemy entity class added.

---
+++ TimeWar/TimeWar.Model/Objects/Classes/Enemy.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="Enemy.cs" company="Time War">
+// Copyright (c) Time War. All rights reserved.
+// </copyright>
+
+namespace TimeWar.Model.Objects.Classes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Enemy character class.
+    /// </summary>
+    public class Enemy : Character
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Enemy"/> class.
+        /// </summary>
+        /// <param name="pos">Position.</param>
+        /// <param name="health">Health value.</param>
+        /// <param name="height">Height.</param>
+        /// <param name="width">Width.</param>
+        /// <param name="spriteFile">Spritesheet file name.</param>
+        public Enemy(Point pos, int health, int height, int width, string spriteFile)
+            : base(pos, health, height, width, spriteFile)
+        {
+        }
+    }
+}
