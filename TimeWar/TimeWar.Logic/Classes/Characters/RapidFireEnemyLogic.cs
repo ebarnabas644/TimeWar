728b05e01eefc1321c01070a191ef29fe99bbbf8
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2021-04-25 15:03:14 +0200

    Added additional enemy logics

---
+++ TimeWar/TimeWar.Logic/Classes/Characters/RapidFireEnemyLogic.cs
@@ -0,0 +1,31 @@
+﻿// <copyright file="RapidFireEnemyLogic.cs" company="Time War">
+// Copyright (c) Time War. All rights reserved.
+// </copyright>
+
+namespace TimeWar.Logic.Classes.Characters
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using TimeWar.Model;
+    using TimeWar.Model.Objects;
+
+    /// <summary>
+    /// Rapid fire enemy.
+    /// </summary>
+    public class RapidFireEnemyLogic : BasicEnemyLogic
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RapidFireEnemyLogic"/> class.
+        /// </summary>
+        /// <param name="model">Game model.</param>
+        /// <param name="character">Character.</param>
+        /// <param name="commandManager">Command manager.</param>
+        public RapidFireEnemyLogic(GameModel model, Character character, CommandManager commandManager)
+            : base(model, character, commandManager)
+        {
+        }
+    }
+}
