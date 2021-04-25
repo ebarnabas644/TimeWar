0721b86d8b1e30ffab25291b1fbaed2615f5223d
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2021-04-25 10:18:25 +0200

    Simplified ActorLogic.

--- TimeWar/TimeWar.Logic/Classes/Characters/ActorLogic.cs
+++ TimeWar/TimeWar.Logic/Classes/Characters/ActorLogic.cs
@@ -18,16 +18,6 @@ namespace TimeWar.Logic.Classes.Characters
     public abstract class ActorLogic
     {
         private const int GravityAcceleration = 5;
-        private int defaultAcceleration;
-        private int maxMovementSpeed;
-        private int maxJumpHeight;
-        private int acceleration;
-        private bool isJumping;
-        private Point moveVector;
-        private BulletType bulletType;
-        private Character character;
-        private CommandManager commandManager;
-        private GameModel model;
         private Stopwatch accelerationStopwatch = new Stopwatch();
         private Stopwatch jumpingTimeOut = new Stopwatch();
         private Stopwatch attackStopwatch = new Stopwatch();
@@ -40,36 +30,28 @@ namespace TimeWar.Logic.Classes.Characters
         /// <param name="commandManager">Command manger.</param>
         protected ActorLogic(GameModel model, Character character, CommandManager commandManager)
         {
-            this.model = model;
-            this.character = character;
-            this.commandManager = commandManager;
-            this.isJumping = false;
-            this.acceleration = this.defaultAcceleration;
+            this.Model = model;
+            this.Character = character;
+            this.CommandManager = commandManager;
+            this.IsJumping = false;
+            this.Acceleration = this.DefaultAcceleration;
             this.jumpingTimeOut.Start();
-            this.moveVector = new Point(0, 0);
-            this.defaultAcceleration = 1;
-            this.maxMovementSpeed = 15;
-            this.maxJumpHeight = 20;
-            this.bulletType = BulletType.Basic;
+            this.MoveVector = new Point(0, 0);
+            this.DefaultAcceleration = 1;
+            this.MaxMovementSpeed = 15;
+            this.MaxJumpHeight = 20;
+            this.BulletType = BulletType.Basic;
         }
 
         /// <summary>
         /// Gets or sets movement vector of an actor.
         /// </summary>
-        protected Point MoveVector
-        {
-            get { return this.moveVector; }
-            set { this.moveVector = value; }
-        }
+        protected Point MoveVector { get; set; }
 
         /// <summary>
         /// Gets or sets bullet type.
         /// </summary>
-        protected BulletType BulletType
-        {
-            get { return this.bulletType; }
-            set { this.bulletType = value; }
-        }
+        protected BulletType BulletType { get; set; }
 
         /// <summary>
         /// Gets or sets if a character can jump again.
@@ -101,74 +83,42 @@ namespace TimeWar.Logic.Classes.Characters
         /// <summary>
         /// Gets or sets gravity acceleration.
         /// </summary>
-        protected int Acceleration
-        {
-            get { return this.acceleration; }
-            set { this.acceleration = value; }
-        }
+        protected int Acceleration { get; set; }
 
         /// <summary>
         /// Gets or sets a value indicating whether the actor is jumping.
         /// </summary>
-        protected bool IsJumping
-        {
-            get { return this.isJumping; }
-            set { this.isJumping = value; }
-        }
+        protected bool IsJumping { get; set; }
 
         /// <summary>
         /// Gets or sets the character.
         /// </summary>
-        protected Character Character
-        {
-            get { return this.character; }
-            set { this.character = value; }
-        }
+        protected Character Character { get; set; }
 
         /// <summary>
         /// Gets or sets the force that is applied when an actor is jumping.
         /// </summary>
-        protected int MaxJumpHeight
-        {
-            get { return this.maxJumpHeight; }
-            set { this.maxJumpHeight = value; }
-        }
+        protected int MaxJumpHeight { get; set; }
 
         /// <summary>
         /// Gets or sets the force that is applied every tick when an actor is moving.
         /// </summary>
-        protected int MaxMovementSpeed
-        {
-            get { return this.maxMovementSpeed; }
-            set { this.maxMovementSpeed = value; }
-        }
+        protected int MaxMovementSpeed { get; set; }
 
         /// <summary>
         /// Gets or sets the force that is applied every tick when a player is moving.
         /// </summary>
-        protected int DefaultAcceleration
-        {
-            get { return this.defaultAcceleration; }
-            set { this.defaultAcceleration = value; }
-        }
+        protected int DefaultAcceleration { get; set; }
 
         /// <summary>
         /// Gets or sets the game model.
         /// </summary>
-        protected GameModel Model
-        {
-            get { return this.model; }
-            set { this.model = value; }
-        }
+        protected GameModel Model { get; set; }
 
         /// <summary>
         /// Gets or sets the command manager.
         /// </summary>
-        protected CommandManager CommandManager
-        {
-            get { return this.commandManager; }
-            set { this.commandManager = value; }
-        }
+        protected CommandManager CommandManager { get; set; }
 
         /// <summary>
         /// One Tick.
@@ -185,7 +135,7 @@ namespace TimeWar.Logic.Classes.Characters
         /// <returns>Tile value.</returns>
         protected int PixelToTile(int num)
         {
-            return this.model.CurrentWorld.ConvertPixelToTile(num);
+            return this.Model.CurrentWorld.ConvertPixelToTile(num);
         }
 
         /// <summary>
@@ -195,7 +145,7 @@ namespace TimeWar.Logic.Classes.Characters
         /// <returns>Pixel value.</returns>
         protected int TileToPixel(int num)
         {
-            return this.model.CurrentWorld.ConvertTileToPixel(num);
+            return this.Model.CurrentWorld.ConvertTileToPixel(num);
         }
 
         /// <summary>
@@ -205,27 +155,27 @@ namespace TimeWar.Logic.Classes.Characters
         /// <returns>True if the movemnt would collide.</returns>
         protected bool GroundCollision(Point newPoint)
         {
-            if (!this.commandManager.IsFinished)
+            if (!this.CommandManager.IsFinished)
             {
                 return false;
             }
 
             Rectangle actor = new Rectangle(
-        this.character.Position.X + newPoint.X,
-        this.character.Position.Y + newPoint.Y,
-        this.character.Width / this.model.CurrentWorld.TileSize,
-        this.character.Height / this.model.CurrentWorld.TileSize);
+        this.Character.Position.X + newPoint.X,
+        this.Character.Position.Y + newPoint.Y,
+        this.Character.Width / this.Model.CurrentWorld.TileSize,
+        this.Character.Height / this.Model.CurrentWorld.TileSize);
             Point actorLocation;
 
             for (int i = 0; i < actor.Width + 1; i++)
             {
-                actorLocation = new Point(this.PixelToTile(actor.X + 1 + this.TileToPixel(i)), this.PixelToTile(actor.Y + this.moveVector.Y) + actor.Height);
-                if (this.model.CurrentWorld.SearchGround(actorLocation))
+                actorLocation = new Point(this.PixelToTile(actor.X + 1 + this.TileToPixel(i)), this.PixelToTile(actor.Y + this.MoveVector.Y) + actor.Height);
+                if (this.Model.CurrentWorld.SearchGround(actorLocation))
                 {
-                    this.moveVector.Y = 0;
-                    this.acceleration = this.defaultAcceleration;
-                    this.accelerationStopwatch.Reset();
-                    this.isJumping = false;
+                    this.SetVectorY(0);
+                    this.Acceleration = this.DefaultAcceleration;
+                    this.AccelerationStopwatch.Reset();
+                    this.IsJumping = false;
                     return true;
                 }
             }
@@ -240,24 +190,24 @@ namespace TimeWar.Logic.Classes.Characters
         /// <returns>True if the movemnt would collide.</returns>
         protected bool TopCollision(Point newPoint)
         {
-            if (!this.commandManager.IsFinished)
+            if (!this.CommandManager.IsFinished)
             {
                 return false;
             }
 
             Rectangle actor = new Rectangle(
-        this.character.Position.X + newPoint.X,
-        this.character.Position.Y,
-        this.character.Width / this.model.CurrentWorld.TileSize,
-        this.character.Height / this.model.CurrentWorld.TileSize);
+        this.Character.Position.X + newPoint.X,
+        this.Character.Position.Y,
+        this.Character.Width / this.Model.CurrentWorld.TileSize,
+        this.Character.Height / this.Model.CurrentWorld.TileSize);
             Point actorLocation;
 
             for (int i = 0; i < actor.Width + 1; i++)
             {
-                actorLocation = new Point(this.PixelToTile(actor.X + 1 + this.TileToPixel(i)), this.PixelToTile(actor.Y + this.moveVector.Y));
-                if (this.model.CurrentWorld.SearchGround(actorLocation))
+                actorLocation = new Point(this.PixelToTile(actor.X + 1 + this.TileToPixel(i)), this.PixelToTile(actor.Y + this.MoveVector.Y));
+                if (this.Model.CurrentWorld.SearchGround(actorLocation))
                 {
-                    this.moveVector.Y = 0;
+                    this.SetVectorY(0);
                     return true;
                 }
             }
@@ -272,8 +222,25 @@ namespace TimeWar.Logic.Classes.Characters
         /// <param name="y">Y direction.</param>
         protected void AddToVector(int x, int y)
         {
-            this.moveVector.X += x;
-            this.moveVector.Y += y;
+            this.MoveVector = new Point(this.MoveVector.X + x, this.MoveVector.Y + y);
+        }
+
+        /// <summary>
+        /// Sets x value of the vector.
+        /// </summary>
+        /// <param name="x">X value.</param>
+        protected void SetVectorX(int x)
+        {
+            this.MoveVector = new Point(x, this.MoveVector.Y);
+        }
+
+        /// <summary>
+        /// Sets y value of the vector.
+        /// </summary>
+        /// <param name="y">Y value.</param>
+        protected void SetVectorY(int y)
+        {
+            this.MoveVector = new Point(this.MoveVector.X, y);
         }
 
         /// <summary>
@@ -284,16 +251,16 @@ namespace TimeWar.Logic.Classes.Characters
         /// <returns>True if the movemnt would collide.</returns>
         protected bool WallCollision(Point newPoint, bool rightWall = true)
         {
-            if (!this.commandManager.IsFinished)
+            if (!this.CommandManager.IsFinished)
             {
                 return false;
             }
 
             Rectangle actor = new Rectangle(
-                this.character.Position.X + newPoint.X,
-                this.character.Position.Y + newPoint.Y,
-                this.character.Width / this.model.CurrentWorld.TileSize,
-                this.character.Height / this.model.CurrentWorld.TileSize);
+                this.Character.Position.X + newPoint.X,
+                this.Character.Position.Y + newPoint.Y,
+                this.Character.Width / this.Model.CurrentWorld.TileSize,
+                this.Character.Height / this.Model.CurrentWorld.TileSize);
             Point actorLocation;
 
             if (rightWall)
@@ -301,10 +268,10 @@ namespace TimeWar.Logic.Classes.Characters
                 // Right wall collision
                 for (int i = 0; i < actor.Height; i++)
                 {
-                    actorLocation = new Point(this.PixelToTile(actor.X + 1 + this.moveVector.X) + actor.Width, this.PixelToTile(actor.Y) + i);
-                    if (this.model.CurrentWorld.SearchGround(actorLocation))
+                    actorLocation = new Point(this.PixelToTile(actor.X + 1 + this.MoveVector.X) + actor.Width, this.PixelToTile(actor.Y) + i);
+                    if (this.Model.CurrentWorld.SearchGround(actorLocation))
                     {
-                        this.moveVector.X = 0;
+                        this.SetVectorX(0);
                         return true;
                     }
                 }
@@ -314,10 +281,10 @@ namespace TimeWar.Logic.Classes.Characters
                 // Left wall collision
                 for (int i = 0; i < actor.Height; i++)
                 {
-                    actorLocation = new Point(this.PixelToTile(actor.X + this.moveVector.X), this.PixelToTile(actor.Y) + i);
-                    if (this.model.CurrentWorld.SearchGround(actorLocation))
+                    actorLocation = new Point(this.PixelToTile(actor.X + this.MoveVector.X), this.PixelToTile(actor.Y) + i);
+                    if (this.Model.CurrentWorld.SearchGround(actorLocation))
                     {
-                        this.moveVector.X = 0;
+                        this.SetVectorX(0);
                         return true;
                     }
                 }
