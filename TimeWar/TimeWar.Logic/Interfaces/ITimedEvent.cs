6afb8532da088f5ffd3b2418868a36cfad67e28b
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2021-04-29 17:48:21 +0200

    Added ITimedEvent interface.

---
+++ TimeWar/TimeWar.Logic/Interfaces/ITimedEvent.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="ITimedEvent.cs" company="Time War">
+// Copyright (c) Time War. All rights reserved.
+// </copyright>
+
+namespace TimeWar.Logic.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Timed event interface.
+    /// </summary>
+    public interface ITimedEvent
+    {
+        /// <summary>
+        /// Resets the player's stats.
+        /// </summary>
+        void ResetStats();
+    }
+}
