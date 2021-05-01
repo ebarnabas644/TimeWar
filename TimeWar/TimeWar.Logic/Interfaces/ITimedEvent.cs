bd9f2c0204f7b92b3b39e316990581c78d3878b7
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2021-05-01 09:57:13 +0200

    Added CheckTimer event.

--- TimeWar/TimeWar.Logic/Interfaces/ITimedEvent.cs
+++ TimeWar/TimeWar.Logic/Interfaces/ITimedEvent.cs
@@ -16,6 +16,12 @@ namespace TimeWar.Logic.Interfaces
     public interface ITimedEvent
     {
         /// <summary>
+        /// Checks if a timed event can despawn.
+        /// </summary>
+        /// <returns>True if timed event is finished.</returns>
+        bool CheckTimer();
+
+        /// <summary>
         /// Resets the player's stats.
         /// </summary>
         void ResetStats();
