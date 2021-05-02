6501c4c0bdf2b12db5fefd4d3cdf3e3fd322cb74
Sipos_Levente_Szabolcs <siposleventeszabolcs@stud.uni-obuda.hu>
2021-05-02 13:25:14 +0200

    Added enemy init logic.

---
+++ TimeWar/TimeWar.Logic/Classes/EnemyInitLogic.cs
@@ -0,0 +1,74 @@
+﻿// <copyright file="EnemyInitLogic.cs" company="Time War">
+// Copyright (c) Time War. All rights reserved.
+// </copyright>
+
+namespace TimeWar.Logic.Classes
+{
+    /// <summary>
+    /// Init model for enemies.
+    /// </summary>
+    public static class EnemyInitLogic
+    {
+        /// <summary>
+        /// Name of the basic enemy spritesheet.
+        /// </summary>
+        public const string BasicEnemySpritesheet = "testenemy";
+
+        /// <summary>
+        /// Name of the fast enemy spritesheet.
+        /// </summary>
+        public const string FastEnemySpritesheet = "fasttestenemy";
+
+        /// <summary>
+        /// Name of the Burst enemy spritesheet.
+        /// </summary>
+        public const string BurstEnemySpritesheet = "bursttestenemy";
+
+        /// <summary>
+        /// Name of the heavy enemy spritesheet.
+        /// </summary>
+        public const string HeavyEnemySpritesheet = "heavytestenemy";
+
+        /// <summary>
+        /// Name of the rapid fire enemy spritesheet.
+        /// </summary>
+        public const string RapidFireEnemySpritesheet = "rapidtestenemy";
+
+        /// <summary>
+        /// Basic enemy height const.
+        /// </summary>
+        public const int BasicEnemyHeight = 32;
+
+        /// <summary>
+        /// Basic enemy width const.
+        /// </summary>
+        public const int BasicEnemyWidth = 16;
+
+        /// <summary>
+        /// Basic enemy health const.
+        /// </summary>
+        public const int BasicEnemyHealth = 75;
+
+        /// <summary>
+        /// Basic enemy health const.
+        /// </summary>
+        ///
+        public const int HeavyEnemyHealth = 200;
+
+        /// <summary>
+        /// Burst enemy health const.
+        /// </summary>
+        ///
+        public const int BurstEnemyHealth = 150;
+
+        /// <summary>
+        /// Basic enemy health const.
+        /// </summary>
+        public const int FastEnemyHealth = 25;
+
+        /// <summary>
+        /// Basic enemy health const.
+        /// </summary>
+        public const int RapidFireEnemyHealth = 150;
+    }
+}
